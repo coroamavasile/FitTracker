996fec20ef4c23fdcd55326a83ff9f484536f86d
Vasile Coroama <vasile.coroama@fabritglobal.com>
2023-10-14 23:16:38 +0300

    change app name

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
-import {BrowserRouter} from 'react-router-dom';
-import {store} from './store/store';
-import {Provider} from 'react-redux';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
     <React.StrictMode>
@@ -15,5 +13,5 @@ root.render(
         <App />
       </BrowserRouter>
     </React.StrictMode>
-  </Provider>
+  </Provider>,
 );
