2e5c33a4c9e12cb8f68148d27b7a7f3b3f8f8499
Vasile Coroama <vasile.coroama@fabritglobal.com>
2023-10-15 14:18:36 +0300

    add-drawer

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,31 @@ import { AppPrivateRoutes } from './utils/private-routes.component';
 import { useAppSelector } from './store';
 import { UserProfilePage } from './pages/user-profile-page/user-profile.page';
 import { AppNavbar } from './components';
+import { AppDrawer } from './components/common/core/app-fitness-navbar/app-drawer.component';
+import './styles.scss';
 
 const App = () => {
   const { token } = useAppSelector((state) => state.authentication);
 
   return (
     <div>
-      <AppNavbar />
+      {/* <AppNavbar /> */}
       <ToastContainer />
       <Routes>
         {/* Public Routes */}
+
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
 
         {/* Private Routes */}
-        <Route element={<AppPrivateRoutes token={token} />}>
+        <Route
+          element={
+            <AppDrawer>
+              <AppPrivateRoutes token={token} />
+            </AppDrawer>
+          }
+        >
           <Route path="/user-profile" element={<UserProfilePage />} />
           <Route path="/dashboard" element={<div>User Dashboard</div>} />
         </Route>
